fa113b287bc67927851ffed2873574b75cc3f5b4
RuslanHale <ruskhal333@gmail.com>
2024-06-10 13:57:03 +0500

    add create and show

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
 from getpass import getpass
 from mysql.connector import connect, Error
 
-try:
-    with connect(
-        host="localhost",
-        user='login',
-        password='zuFyETV0',
-    ) as connection:
-        print(connection)
-except Error as e:
-    print(e)
+def create_db(name_db):
+    try:
+        with connect(
+            host="localhost",
+            user='login',
+            password='zuFyETV0',
+        ) as connection:
+            create_db_query = f"CREATE DATABASE {name_db}"
+            with connection.cursor() as cursor:
+                cursor.execute(create_db_query)
+    except Error as e:
+        print(e)
+
+def show_db():
+    try:
+        with connect(
+                host="localhost",
+                user='root',
+                password='zuFyETV0',
+        ) as connection:
+            show_db_query = "SHOW DATABASES"
+            with connection.cursor() as cursor:
+                cursor.execute(show_db_query)
+                for db in cursor:
+                    print(db)
+    except Error as e:
+        print(e)
+
+create_db('makar')
+show_db()
