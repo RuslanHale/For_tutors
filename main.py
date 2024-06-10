b5e6b2d1d64099386641492fa01760deb25ec7b3
RuslanHale <ruskhal333@gmail.com>
2024-06-10 13:33:14 +0500

    begin

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
+from getpass import getpass
+from mysql.connector import connect, Error
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-
+try:
+    with connect(
+        host="localhost",
+        user='login',
+        password='zuFyETV0',
+    ) as connection:
+        print(connection)
+except Error as e:
+    print(e)
